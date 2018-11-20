058a502d95e10c5232eee1b9d06185a574b3faeb
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-21 01:49:10 +0600

    Fetch data & display on the page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import Weather from "./components/Weather";
 const API_KEY = "0ba5bddf480ccd34702a3d134534e8aa";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
   fetchWeather = async e => {
     e.preventDefault();
 
@@ -17,7 +26,26 @@ class App extends Component {
       `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "You Have To Specify Both The Input  Fields"
+      });
+    }
   };
 
   state = {};
@@ -26,7 +54,14 @@ class App extends Component {
       <div>
         <Title />
         <InputForm fetchWeather={this.fetchWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,20 @@ import React, { Component } from "react";
 class Weather extends Component {
   state = {};
   render() {
-    return <div>Weather Component</div>;
+    return (
+      <div>
+        {this.props.city && this.props.country && (
+          <p>
+            Location: {this.props.city},{this.props.country}
+          </p>
+        )}
+
+        {this.props.temperature && <p> Temperature:{this.props.temperature}</p>}
+        {this.props.humidity && <p> Humidity:{this.props.humidity}</p>}
+        {this.props.description && <p>Description: {this.props.description}</p>}
+        {this.props.error && <p>{this.props.error}</p>}
+      </div>
+    );
   }
 }
 
