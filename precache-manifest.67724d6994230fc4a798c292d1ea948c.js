f26f5a5dab571b49ed66832848581805ed2f7a5b
zarab <musfiqzarab@iut-dhaka.edu>
2018-11-21 04:03:56 +0600

    Updates

---
+++ precache-manifest.67724d6994230fc4a798c292d1ea948c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "ed94207da31be50f5b0b",
+    "url": "/weather-app/static/css/main.d58a6fbc.chunk.css"
+  },
+  {
+    "revision": "b6a1cc22406a5e21d9247e46c0340a16",
+    "url": "/weather-app/static/media/weather.b6a1cc22.jpg"
+  },
+  {
+    "revision": "04d43b87edde1fa0c0ba",
+    "url": "/weather-app/static/js/1.04d43b87.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "ed94207da31be50f5b0b",
+    "url": "/weather-app/static/js/main.ed94207d.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weather-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "04d43b87edde1fa0c0ba",
+    "url": "/weather-app/static/css/1.59894ba7.chunk.css"
+  },
+  {
+    "revision": "1bddeef9b92f909d06e664c1515acfb3",
+    "url": "/weather-app/index.html"
+  }
+];
